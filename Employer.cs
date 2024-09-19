ccd97b9af3d47c86a86cc40280ad282a580039e3
SzDani <szabod@kkszki.hu>
2024-09-19 09:49:25 +0200

    List is ready

--- Employer.cs
+++ Employer.cs
@@ -10,11 +10,18 @@ namespace Employees
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Age { get; set; }
+        public int Age { get; set; }
         public  int Salary { get; set; }
+    
+
         public Employer(string row) 
         {
             string[] darabol = row.Split(';');
+            Id = int.Parse(darabol[0]);
+            Name = darabol[1];
+            Age = int.Parse(darabol[2]);
+            Salary = int.Parse(darabol[3]);
+
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,15 @@ namespace Employees
 {
     internal class Program
     {
+        public static List<Employer> employers = new List<Employer>();
         static void Main(string[] args)
         {
+            StreamReader sr = new StreamReader("tulajdonsagok_100sor.txt");
+            for (int i = 0; i <100; i++)
+            {
+                Employer emp = new Employer(sr.ReadLine());
+                employers.Add(emp);
+            }
         }
     }
 }
