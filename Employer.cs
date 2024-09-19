3892fb3aa481b0dc59ddb7b7ceb9f2ad91abbb00
SzDani <szabod@kkszki.hu>
2024-09-19 09:33:53 +0200

    Employer constuctor is ready

---
+++ Employer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Employees
+{
+    public class Employer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public  int Salary { get; set; }
+        public Employer(string row) 
+        {
+            string[] darabol = row.Split(';');
+        }
+    }
+
+}
